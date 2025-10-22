35bac72b834d025f78615379653516c04bb8b54b
AboutYou1975 <tumbalakun011111@gmail.com>
2025-10-22 12:59:03 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+
+const audio = document.getElementById('myAudio');
+        audio.currentTime = 60; // Atur waktu mulai pada detik ke-30 (ganti sesuai kebutuhan Anda)
+
+        audio.oncanplay = function() {
+            audio.play();
+        }
+
+
+
+
+
+
