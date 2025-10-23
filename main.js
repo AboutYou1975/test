0d26e6e7b10e87241357acfb5259cff71e05e8aa
AboutYou1975 <tumbalakun011111@gmail.com>
2025-10-24 01:21:33 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () =>{
 };
 
 const audio = document.getElementById('myAudio');
-        audio.currentTime = 60; // Atur waktu mulai pada detik ke-30 (ganti sesuai kebutuhan Anda)
+        audio.currentTime = 181; // Atur waktu mulai pada detik ke-30 (ganti sesuai kebutuhan Anda)
 
         audio.oncanplay = function() {
             audio.play();
